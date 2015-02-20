9640c9a2b52750ce9a6786597c51021c3a00943d
Mario Rimann <mario@rimann.org>
2015-02-20 20:11:37 +0100

    [TASK] Prepare the HTML output

--- Packages/Application/Mrimann.AngularDemo/Classes/Mrimann/AngularDemo/Controller/StandardController.php
+++ Packages/Application/Mrimann.AngularDemo/Classes/Mrimann/AngularDemo/Controller/StandardController.php
@@ -11,12 +11,10 @@ use TYPO3\Flow\Annotations as Flow;
 class StandardController extends \TYPO3\Flow\Mvc\Controller\ActionController {
 
 	/**
+	 * Just render the HTML template, nothing more needed in here
+	 *
 	 * @return void
 	 */
 	public function indexAction() {
-		$this->view->assign('foos', array(
-			'bar', 'baz'
-		));
 	}
-
 }
