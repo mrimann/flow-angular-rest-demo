1388bbd86019fbe4cc06df72ef8019b70eadd844
Mario Rimann <mario@rimann.org>
2015-02-23 10:45:36 +0100

    [FEATURE] Periodically update the ideas from the API

--- Packages/Application/Mrimann.AngularDemo/Resources/Public/js/angular-demo.js
+++ Packages/Application/Mrimann.AngularDemo/Resources/Public/js/angular-demo.js
@@ -1,7 +1,21 @@
 var angularDemo = angular.module('angularDemo', ['angularDemoServices']);
 
-angularDemo.controller('IdeaListCtrl', ['$scope', 'Idea', function($scope, Idea) {
-	$scope.ideas = Idea.query();
+angularDemo.controller('IdeaListCtrl', ['$scope', '$interval', 'Idea', function($scope, $interval, Idea) {
+	// initial fetching of the ideas from the API
+	$scope.ideas = [];
+	fetchData();
+
+	// set interval for periodically fetching the data from the API to keep up to date
+	$interval(fetchData, 10000);
+
+	// method to actually fetch the data from the API, the insertion of the data into the
+	// scope is handled in the success method to make sure the API request is initially finished,
+	// this avoids the flickering of the list view
+	function fetchData() {
+		allIdeas = Idea.query(function() {
+			$scope.ideas = allIdeas;
+		});
+	}
 
 	// deleting of ideas
 	$scope.remove = function(idea) {
